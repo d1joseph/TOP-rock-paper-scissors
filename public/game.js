49417f46a60cee3ea7f9c02adf82dc7f4e0e8e38
d1joseph <dhiv.joseph@gmail.com>
2021-09-07 22:38:37 +1000

    Added main game() function and enabled in html. Testing looks good so far. Updated the to do list.

--- public/game.js
+++ public/game.js
@@ -1,12 +1,4 @@
 // game.js
-// to do:
-/*
-1. write game.js
-2. refactor for html dom
-3. test game functions, score and outcomes
-4. integrate anime.js
-5. tests
-*/
 
 // main game logic
 const game = () => {
@@ -16,24 +8,24 @@ const game = () => {
 
     // play game
     const playGame = () => {
-        const rockBtn = document.querySelector(".btn rock");
-        const paperBtn = document.querySelector(".btn paper");
-        const scissorsBtn = document.querySelector(".btn scissors");
+        const rockBtn = document.querySelector(".rock");
+        const paperBtn = document.querySelector(".paper");
+        const scissorsBtn = document.querySelector(".scissors");
         const playerSymbols = [rockBtn, paperBtn, scissorsBtn];
         const machineSymbols = ['rock', 'paper', 'scissors'];
 
-        playersSymbols.forEach(symbol => {
+        playerSymbols.forEach(symbol => {
             symbol.addEventListener('click', function(){
                 
                 // display number of decrementing rounds left
-                const roundsLeft = document.querySelector('.rounds');
+                const roundsLeft = document.querySelector('.rounds-left');
                 rounds++;
                 roundsLeft.innerText = `Rounds left: ${10-rounds}`;
 
-                // machine choice randint -
+                // machine choice randint
                 const randChoice = Math.floor(Math.random() * 3);
                 const machineChoice = machineSymbols[randChoice];
-
+                
                 // determine winner
                 winner(this.innerText, machineChoice);
 
@@ -48,44 +40,44 @@ const game = () => {
 
     // function to determine winner
     const winner = (player,machine) => {
-        const result = document.querySelector('.outcome-msg');
+        const outcomeMsg = document.querySelector('.outcome-msg');
         const playerScoreboard = document.querySelector('.player-scoreboard');
         const machineScoreboard = document.querySelector('.machine-scoreboard');
         player.toLowerCase();
         machine.toLowerCase();
 
         if(player === machine){
-            result.textContent = 'Tie'
+            outcomeMsg.textContent = 'Tie'
         }
         else if(player == 'rock'){
             if(machine == 'paper'){
-                result.textContent = 'Machine wins.';
+                outcomeMsg.textContent = 'Machine wins.';
                 machineScore ++;
                 machineScoreboard.textContent = machineScore;
             }else{
-                result.textContent ='Player wins.';
+                outcomeMsg.textContent ='Player wins.';
                 playerScore++;
                 playerScoreboard.textContent = playerScore;
             }
         }
         else if(player == 'scissors'){
             if(machine == 'rock'){
-                result.textContent = 'Machine wins.';
+                outcomeMsg.textContent = 'Machine wins.';
                 machineScore ++;
-                machineScoreboard.textContent = machineScoreboard;
+                machineScoreboard.textContent = machineScore;
             }else{
-                result.textContent = 'Player wins.';
+                outcomeMsg.textContent = 'Player wins.';
                 playerScore ++;
                 playerScoreboard.textContent = playerScore;
             }
         }
         else if(player == 'paper'){
             if(machine == 'scissors'){
-                result.textContent = 'Machine wins.';
+                outcomeMsg.textContent = 'Machine wins.';
                 machineScore ++;
                 machineScoreboard.textContent = machineScore;
             }else{
-                result.textContent = 'Player wins.';
+                outcomeMsg.textContent = 'Player wins.';
                 playerScore ++;
                 playerScoreboard.textContent = playerScore;
             }
@@ -95,7 +87,7 @@ const game = () => {
     // game over
     const gameOver = (playerSymbols, roundsLeft) => {
         const chooseMove = document.querySelector('.move');
-        const result = document.querySelector('.result');
+        const outcomeMsg = document.querySelector('.outcome-msg');
         // const reloadBtn = document.querySelector('.reload');
 
         playerSymbols.forEach(symbol => {
@@ -103,24 +95,36 @@ const game = () => {
         })
 
         if (playerScore > machineScore){
-            result.style.fontSize = '2rem';
-            result.innerText = 'You win!';
-            result.style.color = '#308D46';
+            outcomeMsg.style.fontSize = '2rem';
+            outcomeMsg.innerText = 'You win!';
+            outcomeMsg.style.color = '#308D46';
         }
         else if(playerScore < machineScore){
-            result.style.fontSize = '2rem';
-            result.innerText = 'You lost.';
-            result.style.color = 'red';
+            outcomeMsg.style.fontSize = '2rem';
+            outcomeMsg.innerText = 'You lost.';
+            outcomeMsg.style.color = 'red';
         }
         else{
-            result.style.fontSize = '2rem';
-            result.innerText = 'Tie game -.-';
-            result.style.color = 'grey';
+            outcomeMsg.style.fontSize = '2rem';
+            outcomeMsg.innerText = 'Tie game -.-';
+            outcomeMsg.style.color = 'grey';
         }
 
     }
 
-    // To do: refactor and integrate with HTML.
+    
+    playGame()
 
-    // playGame();
 }
+game();
+
+/* To do:
+    * gameOver() function added
+    * integrate anime.js library
+    * animate symbols selections to display in .column element 
+    * Add reload button and reload() function to reset the game
+    * styling and layout update
+    * add button wrapper with symbol imgs to button elements
+    * mobile optimisation
+    * testing
+*/
