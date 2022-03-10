ec1a6c94410b5bd01811ff381824e539f7d1c114
d1joseph <dhiv.joseph@gmail.com>
2022-03-11 00:02:00 +1100

    rewriting JS.

--- public/src/render.js
+++ public/src/render.js
@@ -1,10 +1,11 @@
-let render = () => {
+export function render(btn) {
     // left window
     let symbol = document.getElementById("player");
     
-    document.getElementById("rock").onclick = function() {
+    document.getElementById(btn).onclick = function() {
         symbol.classList.toggle("fade");
     }
 };
 
+export default render;
 
