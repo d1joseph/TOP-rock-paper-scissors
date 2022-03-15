2450ca3f8c93be3d3ea97b012caa8a2881f18430
d1joseph <dhiv.joseph@gmail.com>
2022-03-15 16:22:35 +1100

    Added playerChoice() which renders rock, paper or scissors according to the element id clicked.

--- public/game.js
+++ public/game.js
@@ -1,7 +1,28 @@
 // game.js
-import { render } from './src/render.js'; // usage: render(elementId);
 
 let hudMessage = document.querySelector('.message');
+let playerScore = document.querySelector('score-player');
+let machineScore = document.querySelector('score-machine');
+let playerDisplay = document.getElementById('player');
+let machineDisplay = document.getElementById('machine');
+
+
+const symbols = [rock, paper, scissors];
 const rockButton = document.getElementById('rock');
 const paperButton = document.getElementById('paper');
 const scissorsButton = document.getElementById('scissors');
+
+let playerChoice = (buttonId) => {
+    // check the button id and render corrosponding img
+    switch (buttonId) {
+        case 'rock':
+            playerDisplay.src = '../assets/dark/rock.svg';
+            break;
+        case 'paper':
+            playerDisplay.src = '../assets/dark/paper.svg';
+            break
+        case 'scissors':
+            playerDisplay.src = '../assets/dark/scissors.svg';
+            break
+    }
+};
