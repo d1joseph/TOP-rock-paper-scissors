82a00bed3a8660126140349e5758392a1de49b5b
d1joseph <dhiv.joseph@gmail.com>
2022-03-24 20:38:53 +1100

    Playing around with rendering elements in the UI on events

--- public/game.js
+++ public/game.js
@@ -1,22 +1,38 @@
 // game.js
 let reset = false;
 let playerDisplay = document.getElementById('player');
+let message = document.querySelector('.message');
 
 let renderPlayerSelection = (buttonId) => {
     // render appropriate image by id i.e. 'rock' renders rock.svg
     switch (buttonId) {
         case 'rock':
             playerDisplay.src = '../assets/dark/rock.svg';
+            message.innerHTML = buttonId;
             break;
         case 'paper':
             playerDisplay.src = '../assets/dark/paper.svg';
+            message.innerHTML = buttonId;
             break
         case 'scissors':
             playerDisplay.src = '../assets/dark/scissors.svg';
+            message.innerHTML = buttonId;
             break
     }
 };
 
+// game loop
+    // set rounds to play chosen by user; 3, 5, 10
+    // get player selection
+    // randomly generate machine selection
+    // start countdown + reveal in displays
+    // evaluate outcome on reveal
+    // update scores
+    // check rounds left
+    // evaluate game loop. Stop if no rounds left, continue otherwise
+    // if game is over declare winner
+
+    
 // restart game
 document.getElementById('reset').addEventListener('click',function(){
     // returns bool based on user input
@@ -28,5 +44,3 @@ document.getElementById('reset').addEventListener('click',function(){
         location.reload();
     }
 });
-
-
