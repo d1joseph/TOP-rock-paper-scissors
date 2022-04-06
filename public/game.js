50c8db87449e84c500416aa0371094c294229fe3
d1joseph <dhiv.joseph@gmail.com>
2022-04-06 17:29:34 +1000

    tidy up and init main variables needed to play.

--- public/game.js
+++ public/game.js
@@ -1,25 +1,41 @@
 // game.js
-let reset = false;
-let message = document.querySelector('.display__message');
+let cascadeLoad = false;
+const symbols = {
+    'rock':'../assets/dark/rock.svg',
+    'paper':'../assets/dark/paper.svg',
+    'scissors':'../assets/dark/scissors.svg',
+};
+
+let reset = false; // resets the game if true
+let round; // round counter
+let message = document.querySelector('.display__message'); // outputs the evaluated outcome
+
+// symbol image elements
 const player1 = document.getElementById('player');
 const player2 = document.getElementById('machine');
+// scores
+let player1Score = document.getElementById('.player-1__score');
+let player2Score = document.getElementById('.player-2__score');
 
-let renderPlayerSelection = (buttonId) => {
+
+let Play = (buttonId) => {
     // render appropriate image by id i.e. 'rock' renders rock.svg
     switch (buttonId) {
         case 'rock':
             // p1
-            player1.src = '../assets/dark/rock.svg';
+            player1.src = symbols.rock;
             player1.alt = `Player 1 throws...${buttonId}!`
+            
             // p2
             player2.src = '../assets/dark/rock.svg';
             player2.alt = `Player 2 throws...${buttonId}!`
             
             message.innerHTML = buttonId;
             break;
+
         case 'paper':
             // p1
-            player1.src = '../assets/dark/paper.svg';
+            player1.src = symbols.paper;
             player1.alt = `Player 1 throws...${buttonId}!`
             // p2
             player2.src = '../assets/dark/paper.svg';
@@ -27,9 +43,10 @@ let renderPlayerSelection = (buttonId) => {
             
             message.innerHTML = buttonId;
             break
+            
         case 'scissors':
             // p1
-            player1.src = '../assets/dark/scissors.svg';
+            player1.src = symbols.scissors;
             player1.alt = `Player 1 throws...${buttonId}!`
             // p2
             player2.src = '../assets/dark/scissors.svg';
@@ -40,18 +57,10 @@ let renderPlayerSelection = (buttonId) => {
     }
 };
 
+// random choice select method
+
 // game loop
-
-    // set rounds to play chosen by user; 3, 5, 10
-    // get player selection
-    // randomly generate machine selection
-    // start countdown + reveal in displays
-    // evaluate outcome on reveal
-    // update scores
-    // check rounds left
-    // evaluate game loop. Stop if no rounds left, continue otherwise
-    // if game is over declare winner
-
+    
     
 // restart game
 document.getElementById('reset-btn').addEventListener('click',function(){
